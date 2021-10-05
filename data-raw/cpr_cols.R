bf09f16cd6489a46311f0244a833760bdf2d3cce
Joel Nitta <joelnitta@gmail.com>
2021-10-05 09:51:40 +0900

    Use Okabe-Ito colors for endemism

--- data-raw/cpr_cols.R
+++ data-raw/cpr_cols.R
@@ -1,21 +1,52 @@
-# all significance colors are from colorbrewer https://colorbrewer2.org
-cpr_signif_cols <- c(
-	"< 0.01" = "#a50f15", # dark red
-	"< 0.025" = "#de2d26", # light red
-	"not significant" = "#ffffcc", # off-white
-	"> 0.975" = "#2c7fb8", # light blue
-	"> 0.99" = "#253494" # dark blue
-)
+library(RColorBrewer)
+
+# - Randomization test significance (colorbrewer CVD safe)
+colorb_paired <- brewer.pal(n = 8, name = "Paired")
+colorb_ylgn <- brewer.pal(n = 5, name = "YlGn")
+cpr_signif_cols <-
+	c(
+		"< 0.01" = colorb_paired[[6]], #  Dark red
+		"< 0.025" = colorb_paired[[5]], # Light red
+		"not significant" = colorb_ylgn[[1]], # Beige
+		"> 0.975" = colorb_paired[[1]], # Light blue
+		"> 0.99" = colorb_paired[[2]] # Dark blue
+	)
 
 usethis::use_data(cpr_signif_cols, overwrite = TRUE)
 
-# endemism colors are from microshades or colorbrewer (https://karstenslab.github.io/microshades/)
-cpr_endem_cols <- c(
-	"paleo" = "#7DCCFF", # blue (microshades)
-	"neo" = "#F09163", # orange (microshades)
-	"not significant" = "#ffffcc", # off-white (colorbrewer)
-	"mixed" = "#E794C1", # light purple (microshades)
-	"super" = "#CC79A7"  # dark purple (microshades)
-)
+# - CANAPE (Okabe-Ito CVD safe)
+cpr_endem_cols <-
+	c(
+		"paleo" = "#0072B2", # dark blue
+		"neo" = "#D55E00", # red
+		"not significant" = colorb_ylgn[[1]], # Beige
+		"mixed" = "#009E73", # green
+		"super" = "#F0E442" # yellow
+	)
 
 usethis::use_data(cpr_endem_cols, overwrite = TRUE)
+
+# Test the colors, check they are CVD-safe
+# - cpr_signif_cols
+tibble(
+	var_a = names(cpr_signif_cols),
+	var_b = 1:length(cpr_signif_cols),
+	var_c = 1:length(cpr_signif_cols),
+) |>
+	ggplot(aes(x = var_b, y = var_c, color = var_a)) +
+	geom_point(size = 3) +
+	scale_color_manual(values = cpr_signif_cols)
+
+colorBlindness::cvdPlot()
+
+# - cpr_endem_cols
+tibble(
+	var_a = names(cpr_endem_cols),
+	var_b = 1:length(cpr_endem_cols),
+	var_c = 1:length(cpr_endem_cols),
+) |>
+	ggplot(aes(x = var_b, y = var_c, color = var_a)) +
+	geom_point(size = 3) +
+	scale_color_manual(values = cpr_endem_cols)
+
+colorBlindness::cvdPlot()
