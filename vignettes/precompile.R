ce646ec2414408ca2ce48e322e8b26b5e9478c65
Joel Nitta <joelnitta@gmail.com>
2022-10-03 11:57:58 +0900

    Precompile vignettes

---
+++ vignettes/precompile.R
@@ -0,0 +1,15 @@
+# Load package in working state
+# not with library()
+library(devtools)
+library(knitr)
+load_all()
+
+# Precompile vignettes to shorten check time
+vgnts <- c("canape.Rmd", "how-many-rand.Rmd", "parallel.Rmd")
+for (vgnt in vgnts) {
+  knit(paste0("vignettes/", vgnt, ".orig"), paste0("vignettes/", vgnt))
+}
+
+# Move figures into vignettes/ folder
+figs <- list.files(pattern = "vigfig")
+fs::file_move(figs, fs::path("vignettes/", figs))
